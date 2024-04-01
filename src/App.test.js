92eab868fe4b88c7e7abdd011ec41cc724abd591
muwa37 <yond41m3@icloud.com>
2024-04-01 23:34:51 +0500

    Modified first test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('sample', () => {
   render(<App />);
-  const helloWorldEl = screen.getByText('');
+
+  const helloWorldEl = screen.getByText(/hello world/i);
+  const btnEl = screen.getByRole('button');
+  const inputEl = screen.getByPlaceholderText(/type in me.../i);
+
   expect(helloWorldEl).toBeInTheDocument();
+  expect(btnEl).toBeInTheDocument();
+  expect(inputEl).toMatchSnapshot();
 });
