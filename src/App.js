54f1d72f0d3acfb381e0ef60f0177c87b3b00edb
muwa37 <yond41m3@icloud.com>
2024-04-01 23:55:48 +0500

    Added async data test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+
+  const onToggle = () => setToggle(prev => !prev);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 100);
+  }, []);
+
   return (
     <div>
+      {toggle && <div data-testid='toggle-elem'>toggle</div>}
       <h2>hello world</h2>
-      <button>click me</button>
+      <button data-testid='toggle-btn' onClick={onToggle}>
+        click me
+      </button>
       <input type='text' placeholder='type in me...' />
+      {data && <div style={{ color: 'red' }}>data</div>}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('sample', () => {
-  render(<App />);
+describe('test app', () => {
+  test('sample', () => {
+    render(<App />);
 
-  const helloWorldEl = screen.getByText(/hello world/i);
-  const btnEl = screen.getByRole('button');
-  const inputEl = screen.getByPlaceholderText(/type in me.../i);
+    const helloWorldEl = screen.getByText(/hello world/i);
+    const btnEl = screen.getByRole('button');
+    const inputEl = screen.getByPlaceholderText(/type in me.../i);
 
-  expect(helloWorldEl).toBeInTheDocument();
-  expect(btnEl).toBeInTheDocument();
-  expect(inputEl).toMatchSnapshot();
+    expect(helloWorldEl).toBeInTheDocument();
+    expect(btnEl).toBeInTheDocument();
+    expect(inputEl).toMatchSnapshot();
+  });
+
+  test('sample async and styles', async () => {
+    render(<App />);
+    const asyncDataEl = await screen.findByText(/data/i);
+    expect(asyncDataEl).toBeInTheDocument();
+    expect(asyncDataEl).toHaveStyle({ color: 'red' });
+  });
 });
